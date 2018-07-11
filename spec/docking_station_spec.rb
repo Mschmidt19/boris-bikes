1aec3840d16928d9d96f3576b30d0ac58ee19336
Marek <MarekSchmidt@outlook.com>
2018-07-11 12:29:23 +0100

    changed to one line notation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require 'boris_bikes'
 
 describe DockingStation do
-  it 'repsonds to the method "release_bike"' do
-    expect(subject).to respond_to(:release_bike)
-  end
+  it { should respond_to(:release_bike) }
 end
