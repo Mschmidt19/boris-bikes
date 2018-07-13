358b193e2b8c0d3d6f33a666df1e9c966f4c9806
Marek <MarekSchmidt@outlook.com>
2018-07-13 16:41:01 +0100

    Finished challenge 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,34 @@
 require_relative 'bike'
 
 class DockingStation
-  def initialize
+
+  def initialize(capacity = 20)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
-    fail 'There are no available bikes' unless @bikes.length > 0
+    fail 'There are no available bikes' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Capacity is full' unless @bikes.length < 20 
+    fail 'Capacity is full' if full?
     @bikes.push(bike)
     @bikes[@bikes.length - 1]
   end
 
   attr_reader :bikes
+
+  attr_reader :capacity
+  
+  private
+  def full?
+    !!(@bikes.length >= @capacity)
+  end
+
+  def empty?
+    !!@bikes.empty?
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,28 +2,23 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it { should respond_to(:release_bike) }
+  describe '#bikes' do
+    it { is_expected.to respond_to(:bikes) } #test for attr_reader of bikes
 
-  it { is_expected.to respond_to(:bikes) } #test for attr_reader of bike
-
-  #tests for dock method and ensures dock accepts one argument
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it "should release a working bike" do
-    new_bike = Bike.new
-    subject.dock(new_bike)
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it 'should respond to bikes with a currently docked bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes[0]).to eq bike
+    end
   end
 
-
-  it 'should respond to bikes with a currently docked bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes[0]).to eq bike
+  describe '#capacity' do
+    it { is_expected.to respond_to(:capacity) } #test for attr_reader of capacity
   end
 
   describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike) }
+
     it 'raises an error if there are no bikes available' do
       expect {subject.release_bike}.to raise_error('There are no available bikes')
     end
@@ -33,22 +28,51 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
+    #this may be better to put in bikes_spec.rb
+    it "should release a working bike" do
+      new_bike = Bike.new
+      subject.dock(new_bike)
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
   end
 
   describe '#dock' do
+    #tests for dock method and ensures dock accepts one argument
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
     it 'docks a bike' do
       bike = Bike.new
       expect(subject.dock(bike)).to eq bike
     end
 
-    it 'raises an error if there is already 20 bikes docked' do
-      20.times {subject.dock(Bike.new)}
+    it 'when using default capacity, raises an error if there is already 20 bikes docked' do
+      subject.capacity.times {subject.dock(Bike.new)}
       expect {subject.dock(Bike.new)}.to raise_error('Capacity is full')
     end
 
-    it 'can dock 20 bikes' do
-      20.times {subject.dock(Bike.new)}
-      expect(subject.bikes.length).to eq 20
+    it 'when using default capacity, can dock 20 bikes' do
+      subject.capacity.times {subject.dock(Bike.new)}
+      expect(subject.bikes.length).to eq subject.capacity
+    end
+
+    it 'when specifying a capacity of 30, raises an error if there is already 30 bikes docked' do
+      ds_30 = DockingStation.new(30)
+      ds_30.capacity.times {ds_30.dock(Bike.new)}
+      expect {ds_30.dock(Bike.new)}.to raise_error('Capacity is full')
+    end
+
+    it 'when specifying a capacity of 30, does not raise an error when docking 21 bikes' do
+      ds_30 = DockingStation.new(30)
+      20.times {ds_30.dock(Bike.new)}
+      expect {ds_30.dock(Bike.new)}.to_not raise_error
+    end
+
+    it 'when specifying a capacity of 30, can dock 30 bikes' do
+      ds_30 = DockingStation.new(30)
+      ds_30.capacity.times {ds_30.dock(Bike.new)}
+      expect(ds_30.bikes.length).to eq ds_30.capacity
     end
   end
 
